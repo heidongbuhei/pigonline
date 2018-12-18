7299f482c6e1500305a970b212a115235704dc58
liyajing <liyajing6625@163.com>
2018-12-18 12:43:47 +0800

    猪场管家分页完成

--- build/js/home_page.js
+++ build/js/home_page.js
@@ -75,75 +75,11 @@ $(document).ready(function () {
     });
 
 
-    window.onload=function (ev) {
-        showtable();
-    };
-    //渲染表格
-    function showtable() {
-        $.ajax({
-            type: "get",
-            url: "http://10.2.130.251:8086/test/pagination",
-            data: {
-                page:1,
-                page_size:34
-            },
-            async: true,
-            dataType: "json",
-
-            success: function (res) {
-                console.log(res);
-                if(res.code==0){
-                    var str="",
-                        table1=document.getElementById('table1');
-
-                    for(i=0;i<res.data.rows.length;i++){
-                        var earMark=res.data.rows[i].earMark,
-                            position=res.data.rows[i].position,
-                            recordType=res.data.rows[i].recordType,
-                            recordTime=res.data.rows[i].recordTime,
-                            happenedDayNum=res.data.rows[i].happenedDayNum,
-                            daysOverdue=res.data.rows[i].daysOverdue;
-                        console.log(daysOverdue);
-                        str += "<tr>" +
-                            "<td>" + earMark + "</td>" +
-                            "<td>" + position + "</td>" +
-                            "<td>" + recordType + "</td>" +
-                            "<td>" + recordTime + "</td>" +
-                            "<td>" + happenedDayNum + "</td>" +
-                            "<td>" + daysOverdue + "</td>" +
-                            "<td>" +
-                            "<input type=checkbox>" +
-                            "<input type='button' value='登记配种' style='border-radius: 5px;background:#ffe2b6;border:none;outline: none;padding:1px 3px'>" +
-                            "</td>"+
-                            "</tr>";
-                    }
-                    table1.innerHTML=str;
-
-                    //分页
-                    $('.table-btn tbody').paginathing({
-                        perPage:6,
-                        insertAfter:'.table-btn',
-                        pageNumbers:true,
-                        containerClass:'pagination-container pull-left'
-                    });
-
-
-                }
-            },
-            error: function (res) {
-                console.log(res.status);
-            }
-        })
-        // console.log(showtable());
-    }
 
 
 
-    //动态加载页面
-    var url = location.host;//获得本地端口
-    console.log(url);
 
-    //动态获取href属性的值
+    //获取href属性的值
     var pagrUrl
     $(".left-menu-third").on('click','a',function () {
         $(".change-part").empty();
@@ -210,3 +146,125 @@ $(document).ready(function () {
         })
     }
 });
+
+
+
+var pageData = {
+    tempPage:1,
+    pageSize:10,
+    total:0
+}
+$(function () {
+    uploadData();
+    $("#pageSizeSelector").change(function () {
+        pageData.pageSize = $("#pageSizeSelector").val();
+        uploadData();
+    })
+})
+
+
+  // 渲染表格
+
+function uploadData() {
+    var rows;
+    $.ajax({
+        url:"http://10.2.130.251:8086/test/pagination",
+        type:"get",
+        data:{
+            page:pageData.tempPage,
+            page_size:pageData.pageSize
+        },
+        dataType:"json",
+        async:false,
+        success:function (res) {
+            console.log(res);
+            if (res.code == 0){
+                pageData.total = res.data.total;
+                $("#totalNum").text(pageData.total);
+                rows = res.data.rows;
+            } else{
+                alert(res.msg);
+            }
+        },
+        error:function (res) {
+            alert(res.status);
+        }
+    })
+    renderPagination();
+    renderTable(rows);
+}
+
+
+  // 渲染分页器
+
+function renderPagination() {
+    if (pageData.pageSize >= pageData.total) {
+        $(".pagination").hide();
+    }else{
+        $(".pagination").show();
+        var totalPage = Math.ceil(pageData.total / pageData.pageSize);
+        $(".pagination").empty();
+        if (pageData.tempPage != 1) {
+            $(".pagination").append("<li class=\"page-item\">\n" +
+                "                        <a class=\"page-link\" href=\"javascript:switchPage(-1)\" aria-label=\"Previous\">\n" +
+                "                            <span aria-hidden=\"true\">&laquo;</span>\n" +
+                "                            <span class=\"sr-only\">Previous</span>\n" +
+                "                        </a>\n" +
+                "                    </li>");
+        }
+        for (var i = 0; i < totalPage; i++) {
+            var active="";
+            if ((i + 1) == pageData.tempPage) {
+                active = "active";
+            }
+            $(".pagination").append("<li class=\"page-item "+active+"\"><a class=\"page-link\" href=\"javascript:switchPage("+(i+1)+")\">"+(i+1)+"</a></li>");
+        }
+        if (pageData.tempPage != totalPage) {
+            $(".pagination").append("<li class=\"page-item\">\n" +
+                "                        <a class=\"page-link\" href=\"javascript:switchPage(-2)\" aria-label=\"Next\">\n" +
+                "                            <span aria-hidden=\"true\">&raquo;</span>\n" +
+                "                            <span class=\"sr-only\">Next</span>\n" +
+                "                        </a>\n" +
+                "                    </li>");
+        }
+    }
+}
+
+
+  // 渲染表格
+
+function renderTable(rows) {
+    $("#pigTableBody").empty();
+    for (var i = 0; i < rows.length; i++) {
+        $("#pigTableBody").append("<tr>\n" +
+            "                    <th scope=\"row\">"+rows[i].earMark+"</th>\n" +
+            "                    <td>"+rows[i].position+"</td>\n" +
+            "                    <td>"+rows[i].recordType+"</td>\n" +
+            "                    <td>"+rows[i].recordTime+"</td>\n" +
+            "                    <td>"+rows[i].happenedDayNum+"</td>\n" +
+            "                    <td>"+rows[i].daysOverdue+"</td>\n" +
+            "<td>" +
+            "<input type=checkbox>" +
+            "<input type='button' value='登记配种' style='border-radius: 5px;background:#ffe2b6;border:none;outline: none;padding:1px 3px'>" +
+            "</td>"+
+            "                </tr>");
+    }
+}
+
+
+  // 切换页面
+
+function switchPage(page) {
+    if (page == -1) {
+        if (pageData.tempPage > 1) {
+            pageData.tempPage --;
+        }
+    }else if (page == -2) {
+        if(Math.ceil(pageData.total / pageData.pageSize) > pageData.tempPage){
+            pageData.tempPage ++;
+        }
+    }else{
+        pageData.tempPage = page;
+    }
+    uploadData();
+}
